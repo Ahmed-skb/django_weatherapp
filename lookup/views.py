037c5e0acbaf38fd0bf5065220222c104bb44fe1
Ahmed-skb <skb253164@gmail.com>
2020-06-13 03:28:56 +0600

    initialize git

---
+++ lookup/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    context={}
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
