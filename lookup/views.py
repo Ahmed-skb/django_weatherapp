7be2e131ae2fd308fa69eccf94f4bf1dc30a0046
Ahmed-skb <skb253164@gmail.com>
2020-06-13 21:23:40 +0600

    fetching data from api

--- lookup/views.py
+++ lookup/views.py
@@ -4,7 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    context={}
+    import json
+    import requests
+
+    api_request = requests.get(
+        "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=10006&distance=25&API_KEY=ACBFBC5C-3235-4E72-9E00-5B58BA6344D8")
+    
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = 'Error'
+
+    context={'api': api}
     return render(request, 'index.html', context)
 
 
